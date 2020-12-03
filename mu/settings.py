14ea8eb051fbddf0d83f32d1597aa6eadb45bc85
Philippe Martinet <phi.mar.email@gmail.com>
2020-12-03 20:46:21 +0100

    Second deploylment on heroku. fix the secret key and DB setings

--- mu/settings.py
+++ mu/settings.py
@@ -31,8 +31,7 @@ print('BASE_DIR=',BASE_DIR)
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f557wk)rd8_2i!yhgwmyu!ucu2k8=ce0!(df43233%+g_3((-)'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -103,17 +102,6 @@ WSGI_APPLICATION = 'mu.wsgi.application'
 # }
 
 
-# In production use postgresql
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "mu.db",
-        'USER': 'phimar',
-        'PASSWORD': 'Pas2pdtc',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
@@ -153,29 +141,35 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(BASE_DIR), "static", "static")
-]
-ENCRYPT_KEY = b'nLSOyfzWTyEPvkkkKIK5n1jXc2N1IwkV0RvfMTXWbC0='
+
 # Configure Django App for Heroku.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+#STATICFILES_DIRS = [
+#    os.path.join(os.path.dirname(BASE_DIR), "static", "static")
+#]
+
+# If images or other media
+#MEDIA_URL = "/media/"
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 django_heroku.settings(locals())
 
-options=DATABASES['default'].get('OPTIONS',{})
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+#options=DATABASES['default'].get('OPTIONS',{})
+
+ENCRYPT_KEY = b'nLSOyfzWTyEPvkkkKIK5n1jXc2N1IwkV0RvfMTXWbC0='
 options.pop('sslmode',None)
 
-#STATIC_URL = '/static/'
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
